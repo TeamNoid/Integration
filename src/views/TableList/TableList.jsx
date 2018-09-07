e52b44b3fc97cbe0ce1ea551058851c01dc870d1
ralakrose <ralakrose@gmail.com>
2018-09-07 14:28:19 +0300

    Dashboard changes

--- src/views/TableList/TableList.jsx
+++ src/views/TableList/TableList.jsx
@@ -53,15 +53,10 @@ function TableList(props) {
           </CardHeader>
           <CardBody>
             <Table
-              tableHeaderColor="primary"
-              tableHead={["Name", "Country", "City", "Salary"]}
+              tableHeaderColor="info"
+              tableHead={["#", "HTTP method", "Path", "Request Parameters", "Status", "Request Time"]}
               tableData={[
-                ["Dakota Rice", "Niger", "Oud-Turnhout", "$36,738"],
-                ["Minerva Hooper", "Curaçao", "Sinaai-Waas", "$23,789"],
-                ["Sage Rodriguez", "Netherlands", "Baileux", "$56,142"],
-                ["Philip Chaney", "Korea, South", "Overland Park", "$38,735"],
-                ["Doris Greene", "Malawi", "Feldkirchen in Kärnten", "$63,542"],
-                ["Mason Porter", "Chile", "Gloucester", "$78,615"]
+                ["######", "######", "######", "######", "######", "######"]
               ]}
             />
           </CardBody>
