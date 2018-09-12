2526afe5c1ed7dc69934190485da6fc350eaec4c
bikundo <ALLANBMAGETO@GMAIL.COM>
2018-09-12 12:33:20 +0300

    task 2 6 9

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -1,12 +1,9 @@
 // @material-ui/icons
 import Dashboard from "@material-ui/icons/Dashboard";
 import Person from "@material-ui/icons/Person";
-// import ContentPaste from "@material-ui/icons/ContentPaste";
-//import LibraryBooks from "@material-ui/icons/LibraryBooks";
+
 import BubbleChart from "@material-ui/icons/BubbleChart";
-//import LocationOn from "@material-ui/icons/LocationOn";
-// Notifications from "@material-ui/icons/Notifications";
-//import Unarchive from "@material-ui/icons/Unarchive";
+
 import Task1 from "views/Task1/task1.jsx";
 import Task2 from "views/Task2/task2.jsx";
 import Task3 from "views/Task3/task3.jsx";
@@ -68,15 +65,15 @@ const dashboardRoutes = [
     sidebarName: "Task 1",
     navbarName: "Task 1",
     icon: "content_paste",
-    component: Icons
+    component: Task1
   },
 
   {
     path: "/task2",
-    sidebarName: "Task 2",
-    navbarName: "Task 2",
+    sidebarName: "Staffing and Identification",
+    navbarName: "Health Care Staffing and Identification of HIV positive clients",
     icon: "content_paste",
-    component: Icons
+    component: Task2
   },
   {
     path: "/task3",
@@ -101,10 +98,10 @@ const dashboardRoutes = [
   },
   {
     path: "/task6",
-    sidebarName: "Task 6",
-    navbarName: "Task 6",
+    sidebarName: "Training and Prescription practices",
+    navbarName: "Health care worker training and adherance to prescription practices",
     icon: "content_paste",
-    component: Icons
+    component: Task6
   },
   {
     path: "/task7",
@@ -122,10 +119,10 @@ const dashboardRoutes = [
   },
   {
     path: "/task9",
-    sidebarName: "Task 9",
-    navbarName: "Task 9",
+    sidebarName: "Training and uptake of FP",
+    navbarName: "Health worker training and an increased uptake of FP among HIV clients",
     icon: "content_paste",
-    component: Icons
+    component: Task9
   },
   {
     path: "/icons",
